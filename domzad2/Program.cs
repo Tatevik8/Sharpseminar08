e574d5e761b36d2c94ac7c0bc077b133138be9dc
Tatevik8 <tvk25@bk.ru>
2023-03-12 09:33:24 +0300

    Program counts the sum of elements in each row of a two-dimensional array and returns index of the row  with the smallest sum of elements.

---
+++ domzad2/Program.cs
@@ -0,0 +1,72 @@
+﻿// **Задача 56:** Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int SearchingRowNumberWithMinSum(int[,] array)
+{
+    int index = 0;
+    int sum = 0;
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];            
+        }
+        if (i == 0)
+            result = sum;            
+        else if (sum < result)
+        {
+            result = sum;
+            index = i;            
+        }
+    }  
+    return index;    
+}
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+Console.Write($"Индекс строки с наименьшей суммой элементов (отсчет начинается с нуля): ");
+int index = SearchingRowNumberWithMinSum(myMatrix);
+Console.WriteLine(index);
