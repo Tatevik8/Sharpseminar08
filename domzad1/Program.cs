f2da278ae3f0186bf8285fd6d33dadda095f8a14
Tatevik8 <tvk25@bk.ru>
2023-03-12 08:26:13 +0300

    Program will sort the elements of each row of a two-dimensional array in descending order.

---
+++ domzad1/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая  
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+int[,] PrintMatrixPoYbivaniy(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minj = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minj]) minj = k;    
+            }
+            int newarray = array[i, j];                                          
+            array[i, j] = array[i, minj];                            
+            array[i, minj] = newarray;                                    
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+
+int m = ReadInt("Введите количество строк матрицы: ");
+int n = ReadInt("Введите количество столбцов матрицы: ");
+Console.WriteLine($"Сформирован двумерный массив: ");
+var myMatrix = GenerateMatrix(m, n);
+PrintMatrix(myMatrix);
+Console.WriteLine($"Элементы каждой строки двумерного массива, расположенные по убыванию: ");
+int[,] myMatrixPoYbivaniy = PrintMatrixPoYbivaniy(myMatrix);
+PrintMatrix(myMatrix);
